5284a2410eeea6721a4d1fa9d18a9b8cab7f706b
Minesh Patel <mineshpatelis@gmail.com>
2024-03-05 21:31:55 +0000

    Test GET repo call

--- packages/functions/src/github.ts
+++ packages/functions/src/github.ts
@@ -25,16 +25,20 @@ export const handler = ApiHandler(async (event) => {
   const ghApiRerunEndpoint = `https://api.github.com/repos/mpatel/${repo}/actions/runs/${workflowRunId}/rerun-failed-jobs`;
   console.log(ghAccessToken);
   try {
-    const response = await fetch(ghApiRerunEndpoint, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/vnd.github+json',
-        Authorization: `Bearer ${ghAccessToken}`,
-        'X-GitHub-Api-Version': '2022-11-28',
-        'X-OAuth-Scopes': 'repo,workflow',
-        'X-Accepted-OAuth-Scopes': 'repo,workflow',
-      },
-    });
+    const response = await fetch(
+      'https://api.github.com/repos/mpatel/bambinos-story-v2',
+      {
+        // method: 'POST',
+        headers: {
+          Accept: 'application/vnd.github+json',
+          Authorization: `Bearer ${ghAccessToken}`,
+          'X-GitHub-Api-Version': '2022-11-28',
+          //   'X-OAuth-Scopes': 'repo,workflow',
+          //   'X-Accepted-OAuth-Scopes': 'repo,workflow',
+        },
+        method: 'GET',
+      }
+    );
 
     console.log(response);
 
